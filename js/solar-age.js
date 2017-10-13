3e401cf58f65f60ecd86297675c155af8df3f754
Mitchell <meechllada@gmamil.com>
2017-10-13 09:39:45 -0700

    create SolarAge class constructor and successfully link to spec

---
+++ js/solar-age.js
@@ -0,0 +1,8 @@
+export class SolarAge{
+  constructor(){
+  }
+
+  someNewMethod(){
+
+  };
+};
---
+++ spec/solar-calendar-spec.js
@@ -0,0 +1,16 @@
+import { SolarAge } from './../js/solar-age.js';
+
+
+describe('SolarAge', function(){
+
+  beforeEach(function(){
+      let solarAge = new SolarAge()
+    });
+
+  it('should take a persons age and convert it to seconds', function(){
+    expect(true).toEqual(true)
+  });
+
+
+
+});
